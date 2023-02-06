9833ecaee09f161033023ef620a79faa19178c73
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-02-06 22:44:16 +0200

    Task 2

---
+++ src/main/scala/part2/uniqueSum.scala
@@ -0,0 +1,14 @@
+package part2
+
+object uniqueSum {
+  def main(args: Array[String]): Unit = {
+    // initial list containing integers
+    val list :List[Int] = List(1, 2, 4, 8, 4, 2)
+    // new list containing only unique elements of the initial list
+    val uniqueElList :List[Int] = list.distinct
+    // calculate sum of the elements of the new list
+    var sum :Int = uniqueElList.sum
+    // print out the result
+    println("The sum of the unique elements of the list " + list + " is: " + sum)
+  }
+}
