949fe9a81795454d53d7cdd3fb8ae7e2dca07266
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-02-06 21:19:01 +0200

    Task 2

---
+++ src/main/scala/part2/reverse.scala
@@ -0,0 +1,11 @@
+package part2
+
+object reverse {
+  def main(args: Array[String]): Unit = {
+    // list containing integers
+    val list :List[Int] = List(1, 2, 4, 8, 4)
+    // reversed list
+    val reversedList :List[Int] = list.reverse
+    println("The reversed list is: " + reversedList)
+  }
+}
