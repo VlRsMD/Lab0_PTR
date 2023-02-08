9b217fddd8bb739e0708a053bbc208ac41404c43
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-02-08 09:19:53 +0200

    Task 2

--- src/main/scala/part2/mainTasks/caesarCipher.scala
+++ src/main/scala/part2/mainTasks/caesarCipher.scala
@@ -1,5 +1,38 @@
 package part2.mainTasks
 
-object caesarCipher {
+import scala.collection.mutable.ListBuffer
 
+object caesarCipher {
+  def encode (str: String, key :Int): String = {
+    var alph :String = "abcdefghijklmnopqrstuvwxyz"
+    val strArr: Array[String] = str.split("")
+    val ciphertextB :ListBuffer[String] = new ListBuffer
+    var i :Int = 0
+    for (i <- 0 to strArr.length-1) {
+      var k :Int = (alph.indexOf(strArr(i))+key) % 26
+      ciphertextB += alph.charAt(k).toString
+    }
+    val ciphertextL = ciphertextB.toList
+    var ciphertext :String = ciphertextL.mkString("")
+    return ciphertext
+  }
+
+  def decode (str: String, key :Int): String = {
+    var alph :String = "abcdefghijklmnopqrstuvwxyz"
+    val strArr: Array[String] = str.split("")
+    val plaintextB :ListBuffer[String] = new ListBuffer
+    var i :Int = 0
+    for (i <- 0 to strArr.length-1) {
+      var k :Int = (alph.indexOf(strArr(i))-key) % 26
+      plaintextB += alph.charAt(k).toString
+    }
+    val plaintextL = plaintextB.toList
+    var plaintext :String = plaintextL.mkString("")
+    return plaintext
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Plaintext 'lorem' encoded with key 3 produces ciphertext: " + encode("lorem", 3))
+    println("Ciphertext 'oruhp' encoded with key 3 produces plaintext: " + decode("oruhp", 3))
+  }
 }
