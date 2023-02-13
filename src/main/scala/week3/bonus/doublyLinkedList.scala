ba38b304747e464605482dd3b07d8fda398e7230
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-02-13 20:39:28 +0200

    Task 3

---
+++ src/main/scala/week3/bonus/doublyLinkedList.scala
@@ -0,0 +1,76 @@
+package week3.bonus
+
+import akka.actor._
+
+import scala.collection.mutable
+import scala.collection.mutable._
+
+class doublyLinkedListA extends Actor {
+  def receive = {
+    case m :String => println(m)
+  }
+}
+
+object dLL {
+  class node (v :Int) {
+    var value :Int = v
+    var next :node = null
+    var prev :node = null
+  }
+
+  var head :node = _
+
+  def add(new_val :Int): Unit = {
+    val newNode = new node(new_val)
+    // adding node to the beginning, previous is null
+    newNode.prev = null
+    // link the new node to the old list
+    newNode.next = head
+    // previous of head node becomes new node
+    if (head != null) {
+      head.prev = newNode
+    }
+    // move head to point to the new node
+    head = newNode
+  }
+
+  def inverse(): Unit = {
+    val st :Stack[Int] = new mutable.Stack[Int]()
+    var t :node = head
+    while (t != null) {
+      st.push(t.value)
+      t = t.next
+    }
+    t = head
+    while (t != null) {
+      t.value = st.pop()
+      t = t.next
+    }
+  }
+
+  def main(args: Array[String]) {
+    add(78)
+    add(42)
+    add(5)
+    add(4)
+    add(3)
+    val store :node = head
+    println("Doubly linked list traversed: ")
+    while (head != null) {
+      print(head.value + " ")
+      head = head.next
+    }
+    head = store
+    println()
+    inverse()
+    println("Doubly linked list inversed: ")
+    while (head != null) {
+      print(head.value + " ")
+      head = head.next
+    }
+  }
+}
+
+
+
+
