a6d9ef25f477ba7694d94147f9ca5e2e264e5ab0
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-02-27 20:04:17 +0200

    Task 4

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.13.10"
 libraryDependencies += "com.typesafe.akka" % "akka-actor_2.13" % "2.7.0"
 
 libraryDependencies += "com.lihaoyi" %% "requests" % "0.8.0"
+
+libraryDependencies += "org.jsoup" % "jsoup" % "1.15.4"
---
+++ src/main/scala/week5/minimal/collect_quotes.scala
@@ -0,0 +1,23 @@
+package week5.minimal
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+import scala.collection.mutable.ListBuffer
+
+object collect_quotes extends App {
+  var quotes_doc: Document = Jsoup.connect("https://quotes.toscrape.com/").get();
+  val quotes :Elements = quotes_doc.select("div.quote")
+  val quotesListB: ListBuffer[String] = new ListBuffer[String]
+  val quotes_arr = quotes.toArray()
+  for (i<-0 to quotes_arr.length-1) {
+    var quote: Document = Jsoup.parse(quotes_arr(i).toString)
+    var q: String = quote.text()
+    quotesListB += q
+  }
+  val quotesList = quotesListB.toList
+  for (i<-0 to quotesList.length-1) {
+    println(quotesList(i))
+  }
+}
